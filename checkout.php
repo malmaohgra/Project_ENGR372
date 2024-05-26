d53e81bbb47210340169d356715b3a9512758be3
Maram Almaohgra <117986798+malmaohgra@users.noreply.github.com>
2024-05-26 18:29:21 +0300

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -67,7 +67,7 @@ mysqli_close($conn);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Check out</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
     <style>
